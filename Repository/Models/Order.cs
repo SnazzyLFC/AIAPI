93435714a89bdaa1c2bd3a4ac0fe0a8cc8ddc308
gkotl <gkotl@DESKTOP-8J4CPLH>
2017-03-07 15:46:27 +0100

    Repository models added

---
+++ Repository/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Repository.Models
+{
+    public class Order
+    {
+
+        public Order()
+        {
+            Products = new List<Product>();
+        }
+
+        public int OrderId { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Repository/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Repository.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        public int? OrderId { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
